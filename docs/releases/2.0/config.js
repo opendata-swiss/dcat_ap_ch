46b65dee2a48af8497ef4ff449f7f89a122e3a9a
Sabine Maennel <sabine.maennel@gmail.com>
2021-06-07 07:33:05 +0200

    use another respec as template

---
+++ docs/releases/2.0/config.js
@@ -0,0 +1,11 @@
+var respecConfig = {
+  specStatus: "base",
+  editors: [{
+    name: "Sabine Maennel",
+    url: "https://github.com/sabinem",
+  }],
+  github: {
+    branch: "main",
+    repoURL: "https://github.com/opendata-swiss/dcat_ap_ch",
+  },
+};
